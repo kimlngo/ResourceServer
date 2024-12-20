5afb9100d40b564b086e7a7b04d8aab746d27e3a
Kim Long Ngo <ngokimlong2007@gmail.com>
2024-12-19 19:09:34 -0500

    Configure Resource Server

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
@@ -1,6 +1,8 @@
 package com.appsdeveloperblog.ws.api.ResourceServer.controllers;
 
 import com.appsdeveloperblog.ws.api.ResourceServer.response.UserRest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class UsersController {
 
+    @Autowired
+    Environment env;
+
     @GetMapping("/status/check")
     public String status() {
-        return "Working ...";
+        return "Working on port: " + env.getProperty("local.server.port");
     }
 
     @PreAuthorize("hasAuthority('ROLE_developer') or #id == #jwt.subject")
